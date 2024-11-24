263cc01687fab674cbe6499cb7ac0cca8dc07d25
BrokenFinger98 <dbtjsdn980818@gmail.com>
2024-11-24 18:53:24 +0900

    feat: ai 추가

---
+++ src/main/java/ssafy/ssafyhome/ai/presentation/ChatController.java
@@ -0,0 +1,30 @@
+package ssafy.ssafyhome.ai.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RequiredArgsConstructor
+@RequestMapping("/chat")
+@RestController
+public class ChatController {
+
+    private final OpenAiChatModel openAiChatModel;
+
+    @GetMapping
+    public Map<String, String> chat(@RequestBody String message) {
+        Map<String, String> responses = new HashMap<>();
+
+        String openAiResponse = openAiChatModel.call(message);
+        responses.put("openai(chatGPT) 응답", openAiResponse);
+
+        return responses;
+    }
+}
+
